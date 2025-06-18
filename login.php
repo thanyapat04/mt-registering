f1ddc22f8c992ae1a1e402b0baaf519655c1ef3c
thanyapat04 <Thanyapat47@gmail.com>
2025-06-18 16:04:44 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+    <title>เข้าสู่ระบบผู้ดูแล</title>
+</head>
+<body>
+    <form name="loginpage" method="POST" action="register.php">
+        Username: <input type="text" name="username" value="" size="10" />
+        <br>
+        Password: <input type="text" name="password" value="" size="10" />
+        <br>
+        <input type="submit" value="Login"/>	
+     </form>
+  <?php
+  session_start();
+  $db = new PDO('sqlite:meeting.db');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $user = $db->prepare("SELECT * FROM users WHERE username = :username LIMIT 1");
+  $user->bindValue(':username', $username);
+  $user->execute();
+
+  $log = $stmt->fetch(PDO::FETCH_ASSOC);
+  if ($user && password_verify($password, $user['password'])) {
+        // เข้าสู่ระบบสำเร็จ
+        $_SESSION["user"] = $user["username"];
+        header("Location: detail.php"); // เปลี่ยนไปหน้าหลังล็อกอิน
+        exit();
+    } else {
+        $error = "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง";
+    }
+?>
+  
+</body>
+</html>
